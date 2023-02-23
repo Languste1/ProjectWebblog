d1a74a3d64c938801c778e920ac60726eebd8ff7
Bacdasch Zafaryar <bacdasch.zafaryar@brightstraining.com>
2023-02-23 17:34:44 +0100

    GetMapping und Entry Controller erstellt

--- src/main/java/com/webprojekt/webblog/API/EntryController.java
+++ src/main/java/com/webprojekt/webblog/API/EntryController.java
@@ -1,8 +1,26 @@
 package com.webprojekt.webblog.API;
 
+import com.webprojekt.webblog.DAO.Entry;
+import com.webprojekt.webblog.Services.WebBlogServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class EntryController {
+
+    @Autowired
+    WebBlogServices webBlogServices;
+    @GetMapping("/readentry")
+    public @ResponseBody String getEntry (Model model ) {
+        model.addAttribute("Entries", webBlogServices.getEntries());
+
+
+
+    return "Tamayo ist der Beste!!!!!!!!!!!!";
+    }
+
 
 }
