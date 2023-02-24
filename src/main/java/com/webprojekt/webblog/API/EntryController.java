6999e7bd911e23c5e4e4eb236621b768a54f3fbd
ChristianLange <christian.lange@brightstraining.com>
2023-02-24 16:57:20 +0100

    added possibility to add comments

--- src/main/java/com/webprojekt/webblog/API/EntryController.java
+++ src/main/java/com/webprojekt/webblog/API/EntryController.java
@@ -1,12 +1,12 @@
 package com.webprojekt.webblog.API;
 
+import com.webprojekt.webblog.DAO.Comment;
 import com.webprojekt.webblog.DAO.Entry;
 import com.webprojekt.webblog.Services.WebBlogServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class EntryController {
@@ -15,39 +15,35 @@ public class EntryController {
     WebBlogServices webBlogServices;
 
 
-
     @GetMapping("/Entry")
-    public  String getEntry (Model model ) {
+    public String getEntry(Model model) {
 
-
-        webBlogServices.addUser ("Peter");
-        webBlogServices.addUser ("Hanz");
-        webBlogServices.addUser ("Kevin");
-        webBlogServices.addEntry ("Hier ist ein Text1",1L);
-        webBlogServices.addEntry ("Hier ist ein Text2",2L);
-        webBlogServices.addEntry ("Hier ist ein Text3",3L);
-        webBlogServices.addEntry ("Hier ist ein Text4",1L);
+        webBlogServices.addUser("Peter");
+        webBlogServices.addUser("Hanz");
+        webBlogServices.addUser("Kevin");
+        webBlogServices.addEntry("Hier ist ein Text1", 1L);
+        webBlogServices.addEntry("Hier ist ein Text2", 2L);
+        webBlogServices.addEntry("Hier ist ein Text3", 3L);
+        webBlogServices.addEntry("Hier ist ein Text4", 1L);
 
         webBlogServices.addComment("testkommentar1", 1L, 1L);
 
-
-
-
-
-
-
-        model.addAttribute("entries", webBlogServices.getEntriesByCreationDate ());
-
-
+        model.addAttribute("entries", webBlogServices.getEntriesByCreationDate());
         model.addAttribute("comments", webBlogServices.getCommentsByCreationDate());
-  /*
+        model.addAttribute("comment", new Comment());
 
-         */
+        return "entries";
+    }
+
+    @PostMapping("/Entry")
+    public String addComment(@ModelAttribute Comment comment, Model model) {
+
+
+        this.webBlogServices.addComment(comment.getText(), 1L, 2L);
+        model.addAttribute("comment", new Comment());
 
 
 
         return "entries";
     }
-
-
 }
